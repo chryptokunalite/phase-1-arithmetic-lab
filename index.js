5f168c50f7c5deea0b2571bda39935942f47d14c
Allan Saenz <allan.d.saenz@gmail.com>
2023-04-26 19:40:56 -0400

    done

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1
+console.log(random)
+
+let num3 = 18
+let num4 = 7
+const mod = num3 % num4;
+
+const max = Math.max(1,2,3,4,5,6,7,8,9,20)
